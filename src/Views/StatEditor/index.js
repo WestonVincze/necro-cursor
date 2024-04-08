1a0de3edae43a6bdfd0ce23f8677a7d390bc590c
Weston Vincze <westonvincze@gmail.com>
2024-04-08 19:16:12 -0400

    Adds a crude stat editor

--- src/Views/StatEditor/index.js
+++ src/Views/StatEditor/index.js
@@ -1,64 +1,86 @@
 import { units } from "../../data/units";
 import styles from "./index.module.css";
 
-const StatEditorScreen = ({ name, stats }) => `
-<div class="${styles.statEditor}"
+const StatEditorScreen = () => `
+<div class="${styles.statEditor}">
   <form id="stat_editor_form">
-    <div class="${styles.inputGroup}">
-      <label for="unit_selector">Choose a unit to modify</label>
-      <select id="unit_selector">
-        <option value="naked" ${name === "naked" ? "selected" : ""}>Player</option>
-        <option value="skeleton">Skeleton</option>
-        <option value="guard">Guard</option>
-        <option value="paladin">Paladin</option>
-      </select>
-    </div>
-    <div class="${styles.inputGroup}">
-      <label for="maxHP">Max HP:</label>
-      <input type="text" id="maxHP" value="${stats.maxHP}" placeholder="max hp"></input>
-    </div>
-    <div class="${styles.inputGroup}">
-      <label for="armor">Armor:</label>
-      <input type="text" id="armor" value=${stats.armor} placeholder=""></input>
-    </div>
-    <div class="${styles.inputGroup}">
-      <label for="hp_regen">HP Regeneration:</label>
-      <input type="text" id="hp_regen" value=${stats.HPregeneration || 0} placeholder=""></input>
-    </div>
-    <div class="${styles.inputGroup}">
-      <label for="maxHP">Max HP:</label>
-      <input type="text" id="" value=${stats.moveSpeed || 0} placeholder=""></input>
-    </div>
-    <div class="${styles.inputGroup}">
-      <label for="maxHP">Max HP:</label>
-      <input type="text" id="" value=${stats.maxSpeed || 0} placeholder=""></input>
-    </div>
-    <div class="${styles.inputGroup}">
-      <label for="maxHP">Max HP:</label>
-      <input type="text" id="" value=${stats.attackRange || 0} placeholder=""></input>
-    </div>
-    <button type="submit">Save</button>
+    <h2>Unit Stat Editor</h2>
+    <select id="unit_selector">
+      <option value="">Select a Unit</option>
+      <option value="naked">Player</option>
+      <option value="skeleton">Skeleton</option>
+      <option value="guard">Guard</option>
+      <option value="paladin">Paladin</option>
+    </select>
+    <div id="stats" class="${styles.statInputs}"></div>
+    <button type="submit" id="submit" class="hidden">Save</button>
   </form>
 </div>
 `;
 
 export const StatEditor = (container) => {
-  // const overlay = document.querySelector('#overlay');
-  // overlay.classList.add("show");
-  container.innerHTML = StatEditorScreen({ ...units.naked });
-  console.log(units.skeleton)
+  let selectedUnit = null;
+  let modifiedStats = {};
+  container.innerHTML = StatEditorScreen();
+  const statsContainer = document.getElementById('stats');
+
+  const addStatInput = (stat, value) => {
+    const inputGroup = document.createElement("div");
+    inputGroup.classList.add(styles.inputGroup);
+
+    const label = document.createElement("label");
+    label.setAttribute('for', stat);
+    label.textContent = stat;
+
+    const input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', stat);
+    input.setAttribute('value', value);
+
+    input.addEventListener('change', (e) => {
+      const newValue = parseInt(e.target.value);
+      if (selectedUnit[stat] === newValue) return;
+
+      modifiedStats[stat] = newValue;
+    });
+
+    inputGroup.appendChild(label);
+    inputGroup.appendChild(input);
+
+    statsContainer.append(inputGroup);
+  }
 
   const unitSelector = document.getElementById('unit_selector')
+
   unitSelector.addEventListener("change", (e) => {
-    console.log(units[e.target.value].stats)
+    statsContainer.innerHTML = "";
     if (units[e.target.value]) {
-      container.innerHTML = StatEditorScreen({ ...units[e.target.value] });
+      document.getElementById('submit').classList.remove('hidden');
+      selectedUnit = units[e.target.value];
+      const { stats } = selectedUnit;
+      for (const [stat, value] of Object.entries(stats)) {
+        addStatInput(stat, value);
+      }
+    } else {
+      selectedUnit = null;
+      document.getElementById('submit').classList.add('hidden');
     }
   });
 
   const form = document.getElementById('stat_editor_form');
-  form.addEventListener('submit', () => {
-    unitStats.maxHP = parseInt(document.getElementById('maxHP').value);
-    unitStats.armor = parseInt(document.getElementById('armor').value);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (Object.keys(modifiedStats).length > 0) {
+      Object.keys(modifiedStats).map(stat => {
+        selectedUnit.stats[stat] = modifiedStats[stat];
+      })
+
+      statsContainer.innerHTML = `Saved stats for ${selectedUnit.name}!`;
+      unitSelector.value = "";
+      selectedUnit = null;
+      document.getElementById('submit').classList.add('hidden');
+    } 
   });
 }
